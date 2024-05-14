f238a7d9a88cd18fc3013254a14ce6565ce45748
pooh <cktjdgus45@naver.com>
2024-05-14 20:40:50 +0900

    feat:네이버 쇼핑API에서 product data 수집

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,20 @@
 export default function Home() {
+  const myHeaders = new Headers();
+  const naverClientId = process.env.NAVER_CLIENT_ID as string;
+  const naverClientSecret = process.env.NAVER_CLIENT_SECRET as string;
+  myHeaders.append("X-Naver-Client-Id", naverClientId);
+  myHeaders.append("X-Naver-Client-Secret", naverClientSecret);
+
+  const requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow" as RequestRedirect
+  };
+
+  fetch("https://openapi.naver.com/v1/search/shop.json?query=강아지&display=10", requestOptions)
+    .then((response) => response.json())
+    .then((result) => console.log(result))
+    .catch((error) => console.error(error));
   return (
     <>
       home
